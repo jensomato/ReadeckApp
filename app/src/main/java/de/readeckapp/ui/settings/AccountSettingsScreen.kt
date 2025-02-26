b295467559c40668bff872e5cb85a15a6899e593
Jens Dürr <jens@jensomato.de>
2025-02-26 15:00:06 +0100

    add input validation to AccountSettings

--- app/src/main/java/de/readeckapp/ui/settings/AccountSettingsScreen.kt
+++ app/src/main/java/de/readeckapp/ui/settings/AccountSettingsScreen.kt
@@ -2,27 +2,85 @@ package de.readeckapp.ui.settings
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
+import de.readeckapp.R
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AccountSettingsScreen(
     navHostController: NavHostController
 ) {
     val viewModel: AccountSettingsViewModel = hiltViewModel()
     val settingsUiState = viewModel.uiState.collectAsState().value
+    val navigationEvent = viewModel.navigationEvent.collectAsState()
+    val onUrlChanged: (String) -> Unit = { url -> viewModel.onUrlChanged(url)}
+    val onUsernameChanged: (String) -> Unit = { username -> viewModel.onUsernameChanged(username)}
+    val onPasswordChanged: (String) -> Unit = { password -> viewModel.onPasswordChanged(password)}
+    val onLoginClicked: () -> Unit = { viewModel.login() }
+    val onClickBack: () -> Unit = { viewModel.onClickBack() }
 
+    LaunchedEffect(key1 = navigationEvent.value) {
+        navigationEvent.value?.let { event ->
+            when (event) {
+                is AccountSettingsViewModel.NavigationEvent.NavigateBack -> {
+                    navHostController.popBackStack()
+                }
+            }
+            viewModel.onNavigationEventConsumed() // Consume the event
+        }
+    }
+    AccountSettingsView(
+        modifier = Modifier,
+        settingsUiState = settingsUiState,
+        onUrlChanged = onUrlChanged,
+        onUsernameChanged = onUsernameChanged,
+        onPasswordChanged = onPasswordChanged,
+        onLoginClicked = onLoginClicked,
+        onClickBack = onClickBack
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AccountSettingsView(
+    modifier: Modifier = Modifier,
+    settingsUiState: AccountSettingsUiState,
+    onUrlChanged: (String) -> Unit,
+    onUsernameChanged: (String) -> Unit,
+    onPasswordChanged: (String) -> Unit,
+    onLoginClicked: () -> Unit,
+    onClickBack: () -> Unit
+) {
     Scaffold(
-        topBar = { TopAppBar(title = { Text("Settings") }) }
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(stringResource(R.string.accountsettings_topbar_title)) },
+                        navigationIcon = {
+                    IconButton(
+                        onClick = onClickBack,
+                        modifier = Modifier.testTag(AccountSettingsScreenTestTags.BACK_BUTTON)
+                    ) {
+                        Icon(
+                            Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = stringResource(R.string.back)
+                        )
+                    }
+                }
+            )
+        }
     ) { padding ->
         Column(
             modifier = Modifier
@@ -33,42 +91,85 @@ fun AccountSettingsScreen(
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             OutlinedTextField(
-                value = settingsUiState.url,
-                placeholder = { Text("Readeck URL")},
-                onValueChange = { viewModel.onUrlChanged(it) },
-                label = { Text("Backend URL") },
-                modifier = Modifier.fillMaxWidth()
+                value = settingsUiState.url ?: "",
+                placeholder = { Text(stringResource(R.string.account_settings_url_placeholder))},
+                onValueChange = { onUrlChanged(it) },
+                label = { Text(stringResource(R.string.account_settings_url_label)) },
+                modifier = Modifier.fillMaxWidth(),
+                isError = settingsUiState.urlError != null,
+                supportingText = {
+                    settingsUiState.urlError?.let {
+                        Text(text = stringResource(it))
+                    }
+                }
             )
             OutlinedTextField(
-                value = settingsUiState.username,
-                placeholder = { Text("Username")},
-                onValueChange = { viewModel.onUsernameChanged(it) },
-                label = { Text("Username") },
-                modifier = Modifier.fillMaxWidth()
+                value = settingsUiState.username ?: "",
+                placeholder = { Text(stringResource(R.string.account_settings_username_placeholder))},
+                onValueChange = { onUsernameChanged(it) },
+                label = { Text(stringResource(R.string.account_settings_username_label)) },
+                modifier = Modifier.fillMaxWidth(),
+                isError = settingsUiState.usernameError != null,
+                supportingText = {
+                    settingsUiState.usernameError?.let {
+                        Text(text = stringResource(it))
+                    }
+                }
             )
             OutlinedTextField(
-                value = settingsUiState.password,
-                placeholder = { Text("Password")},
-                onValueChange = { viewModel.onPasswordChanged(it) },
-                label = { Text("Password") },
-                visualTransformation = androidx.compose.ui.text.input.PasswordVisualTransformation(),
+                value = settingsUiState.password ?: "",
+                placeholder = { Text(stringResource(R.string.account_settings_password_placeholder))},
+                onValueChange = { onPasswordChanged(it) },
+                label = { Text(stringResource(R.string.account_settings_password_label)) },
+                visualTransformation = PasswordVisualTransformation(),
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth(),
+                isError = settingsUiState.passwordError != null,
+                supportingText = {
+                    settingsUiState.passwordError?.let {
+                        Text(text = stringResource(it))
+                    }
+                }
             )
             Button(
-                onClick = {
-                    viewModel.saveSettings()
-                }
+                onClick = onLoginClicked,
+                enabled = settingsUiState.loginEnabled
             ) {
-                Text("Save Settings")
+                Text(stringResource(R.string.account_settings_login))
             }
         }
     }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun AccountSettingsScreenPreview() {
-    val navHostController = rememberNavController()
-    AccountSettingsScreen(navHostController)
+fun AccountSettingsScreenViewPreview() {
+    val settingsUiState = AccountSettingsUiState(
+        url = "https://example.com",
+        username = "user",
+        password = "pass",
+        loginEnabled = true,
+        urlError = R.string.account_settings_url_error,
+        usernameError = null,
+        passwordError = null
+    )
+    AccountSettingsView(
+        modifier = Modifier,
+        settingsUiState = settingsUiState,
+        onUrlChanged = {},
+        onUsernameChanged = {},
+        onPasswordChanged = {},
+        onLoginClicked = {},
+        onClickBack = {}
+    )
+}
+
+object AccountSettingsScreenTestTags {
+    const val BACK_BUTTON = "AccountSettingsScreenTestTags.BackButton"
+    const val TOPBAR = "AccountSettingsScreenTestTags.TopBar"
+    const val SETTINGS_ITEM = "AccountSettingsScreenTestTags.SettingsItem"
+    const val SETTINGS_ITEM_TITLE = "AccountSettingsScreenTestTags.SettingsItem.Title"
+    const val SETTINGS_ITEM_SUBTITLE = "AccountSettingsScreenTestTags.SettingsItem.Subtitle"
+    const val SETTINGS_ITEM_ACCOUNT = "Account"
 }
--- app/src/main/java/de/readeckapp/ui/settings/AccountSettingsViewModel.kt
+++ app/src/main/java/de/readeckapp/ui/settings/AccountSettingsViewModel.kt
@@ -3,15 +3,16 @@ package de.readeckapp.ui.settings
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import androidx.compose.runtime.mutableStateOf
-import de.readeckapp.domain.model.Bookmark
+import de.readeckapp.R
 import de.readeckapp.domain.usecase.AuthenticateUseCase
 import de.readeckapp.io.prefs.SettingsDataStore
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.net.URL
 import javax.inject.Inject
 
 @HiltViewModel
@@ -19,49 +20,108 @@ class AccountSettingsViewModel @Inject constructor(
     private val settingsDataStore: SettingsDataStore,
     private val authenticateUseCase: AuthenticateUseCase
 ) : ViewModel() {
-    val bookmarks = mutableStateOf<List<Bookmark>>(emptyList())
-    private val _uiState = MutableStateFlow(AccountSettingsUiState("", "", ""))
+    private val _navigationEvent = MutableStateFlow<NavigationEvent?>(null)
+    val navigationEvent: StateFlow<NavigationEvent?> = _navigationEvent.asStateFlow()
+    private val _uiState = MutableStateFlow(AccountSettingsUiState("", "", "", false, null, null, null))
     val uiState = _uiState.asStateFlow()
 
     init {
         viewModelScope.launch {
             _uiState.value = AccountSettingsUiState(
-                settingsDataStore.urlFlow.value ?: "",
-                settingsDataStore.usernameFlow.value ?: "",
-                "pass"
+                url = settingsDataStore.urlFlow.value,
+                username = settingsDataStore.usernameFlow.value,
+                password = null,
+                loginEnabled = false,
+                urlError = null,
+                usernameError = null,
+                passwordError = null
             )
 
         }
     }
 
-    fun saveSettings() {
+    fun login() {
         viewModelScope.launch {
             val success = authenticateUseCase.execute(
-                _uiState.value.url,
-                _uiState.value.username,
-                _uiState.value.password
+                _uiState.value.url!!,
+                _uiState.value.username!!,
+                _uiState.value.password!!
             )
             Timber.d("success=$success")
         }
     }
 
     fun onUrlChanged(value: String) {
-        _uiState.update { it.copy(url = value) }
+        val isValidUrl = try {
+            URL(value)
+            true
+        } catch (e: Exception) {
+            false
+        }
+        val urlError = if (!isValidUrl && value.isNotEmpty()) {
+            R.string.account_settings_url_error // Use resource ID
+        } else {
+            null
+        }
+        _uiState.update {
+            it.copy(
+                url = value,
+                urlError = urlError,
+                loginEnabled = isValidUrl && !it.username.isNullOrBlank() && !it.password.isNullOrBlank()
+            )
+        }
     }
 
     fun onUsernameChanged(value: String) {
-        _uiState.update { it.copy(username = value) }
+        val usernameError = if (value.isBlank()) {
+            R.string.account_settings_username_error // Use resource ID
+        } else {
+            null
+        }
+        _uiState.update {
+            it.copy(
+                username = value,
+                usernameError = usernameError,
+                loginEnabled = !value.isBlank() && !it.url.isNullOrBlank() && !it.password.isNullOrBlank()
+            )
+        }
     }
 
     fun onPasswordChanged(value: String) {
-        _uiState.update { it.copy(password = value) }
+        val passwordError = if (value.isBlank()) {
+            R.string.account_settings_password_error // Use resource ID
+        } else {
+            null
+        }
+        _uiState.update {
+            it.copy(
+                password = value,
+                passwordError = passwordError,
+                loginEnabled = !value.isBlank() && !it.url.isNullOrBlank() && !it.username.isNullOrBlank()
+            )
+        }
     }
 
+    fun onNavigationEventConsumed() {
+        _navigationEvent.update { null } // Reset the event
+    }
+
+    fun onClickBack() {
+        _navigationEvent.update { NavigationEvent.NavigateBack }
+    }
+
+    sealed class NavigationEvent {
+        data object NavigateBack : NavigationEvent()
+    }
 
 }
 
 data class AccountSettingsUiState(
-    val url: String,
-    val username: String,
-    val password: String
+    val url: String?,
+    val username: String?,
+    val password: String?,
+    val loginEnabled: Boolean,
+    val urlError: Int?,
+    val usernameError: Int?,
+    val passwordError: Int?
 )
