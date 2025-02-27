de48b9c9ba68d64b3255d0f05edf25972e448bc6
Jens Dürr <jens@jensomato.de>
2025-02-27 15:30:48 +0100

    feat: Handle authentication results and save password in datastore

--- app/src/main/java/de/readeckapp/io/prefs/SettingsDataStore.kt
+++ app/src/main/java/de/readeckapp/io/prefs/SettingsDataStore.kt
@@ -5,8 +5,10 @@ import kotlinx.coroutines.flow.StateFlow
 interface SettingsDataStore {
     val tokenFlow: StateFlow<String?>
     val usernameFlow: StateFlow<String?>
+    val passwordFlow: StateFlow<String?>
     val urlFlow: StateFlow<String?>
     fun saveUsername(username: String)
+    fun savePassword(password: String)
     fun saveToken(token: String)
     fun saveUrl(url: String)
 }
