359b6aa56f9c46766664859601fe49c26d051b3d
Jens Dürr <jens@jensomato.de>
2025-03-03 11:37:19 +0100

    base64 approach

--- app/src/main/java/de/readeckapp/ui/detail/BookmarkDetailScreen.kt
+++ app/src/main/java/de/readeckapp/ui/detail/BookmarkDetailScreen.kt
@@ -1,6 +1,5 @@
 package de.readeckapp.ui.detail
 
-import android.text.Html
 import android.view.View
 import android.webkit.WebView
 import androidx.compose.foundation.layout.*
@@ -21,6 +20,11 @@ import coil3.compose.AsyncImage
 import coil3.request.ImageRequest
 import coil3.request.crossfade
 import de.readeckapp.domain.model.Bookmark
+import timber.log.Timber
+import java.io.InputStreamReader
+import java.nio.charset.Charset
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -66,8 +70,10 @@ fun BookmarkDetailScreen(navController: NavController, bookmarkId: String?) {
     }
 }
 
+@OptIn(ExperimentalEncodingApi::class)
 @Composable
 fun BookmarkDetailContent(bookmark: Bookmark, modifier: Modifier = Modifier) {
+    val assetManager = LocalContext.current.assets
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -93,9 +99,6 @@ fun BookmarkDetailContent(bookmark: Bookmark, modifier: Modifier = Modifier) {
         }
         Spacer(modifier = Modifier.height(16.dp))
 
-        // Main Section (HTML Content - Placeholder)
-//        Text(text = bookmark.articleContent ?: "HTML", style = MaterialTheme.typography.bodyMedium)
-        // TODO: Implement HTML rendering using a WebView or other suitable component
         AndroidView(
             factory = { context ->
                 WebView(context).apply {
@@ -103,67 +106,20 @@ fun BookmarkDetailContent(bookmark: Bookmark, modifier: Modifier = Modifier) {
                     settings.useWideViewPort = false
                     settings.loadWithOverviewMode = false
                     setLayerType(View.LAYER_TYPE_HARDWARE, null)
+                    settings.defaultTextEncodingName = "utf-8"
                 }
             },
             update = {
                 if (bookmark.hasArticle) {
                     if (bookmark.articleContent != null) {
-                        val htmlContent = Html.fromHtml(bookmark.articleContent, Html.FROM_HTML_MODE_LEGACY).toString()
-                        val content =
-                        """
-                            <html>
-                                <head>
-                                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                                    <style>
-                                        img {
-                                            max-width: 100%; /* Make images responsive */
-                                            height: auto; /* Maintain aspect ratio */
-                                            display: block;  /* prevents space below image */
-                                            margin: 0 auto;  /* Center the image */
-                                        }
-                                        /* Styles for smaller screens (e.g., phones) */
-                                        @media (max-width: 600px) {
-                                            body {
-                                                font-size: 16px; /* Increase font size on small screens */
-                                            }
-
-                                            /* Example:  Adjust the padding */
-                                            /* Some CSS to prevent over-scrolling if content is bigger than screen width */
-                                            .container {
-                                                width: 100%;
-                                                padding: 10px; /* Adjust padding on smaller screens */
-                                            }
-                                        }
-
-                                        /* Styles for larger screens (e.g., tablets and desktops) */
-                                        @media (min-width: 601px) {
-                                            body {
-                                                font-size: 14px;
-                                            }
-
-                                            /* Example: Wider content area */
-                                            .container {
-                                                max-width: 800px;  /* Limit width on larger screens */
-                                                margin: 0 auto; /* Center the container */
-                                                padding: 20px;
-                                            }
-
-                                             /* Example: Reduce image size */
-                                            img {
-                                                max-width: 80%; /* Smaller images on larger screens */
-                                            }
-
-                                        }
-                                    </style>
-                                </head>
-                                <body>
-                                    <div class="container">
-                                    ${bookmark.articleContent}
-                                    </div>
-                                </body>
-                            </html>
-                        """
-                        it.loadData(content, "text/html", "utf-8")
+                        val htmlContent = bookmark.articleContent
+                        val inputStream = assetManager.open("html_template.html")
+                        val reader = InputStreamReader(inputStream)
+                        val template = reader.readText()
+                        reader.close()
+                        val content = template.replace("%s", htmlContent)
+                        val encodedHtml = Base64.withPadding(Base64.PaddingOption.ABSENT).encode(content.toByteArray())
+                        it.loadData(encodedHtml, "text/html", "base64")
                     }
                 }
             }
