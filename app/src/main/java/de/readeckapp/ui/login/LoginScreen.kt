465e273daa2da0c6c64b952fcd4ae3e14f8ad5b8
Sockenklaus <sockenklaus@gmail.com>
2025-06-06 00:06:26 +0200

    Worked mostly on the design of `LoginScreen.kt` and started adding some first business logic to `LoginScreenViewModel.kt`

--- app/src/main/java/de/readeckapp/ui/login/LoginScreen.kt
+++ app/src/main/java/de/readeckapp/ui/login/LoginScreen.kt
@@ -1,99 +1,183 @@
 package de.readeckapp.ui.login
 
-import android.widget.ImageView
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Switch
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.outlined.Cloud
+import androidx.compose.material.icons.outlined.Password
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import de.readeckapp.R
-
+import androidx.hilt.navigation.compose.hiltViewModel
+import de.readeckapp.ui.components.AdaptiveReadeckIcon
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen(navHostController: NavHostController) {
-    Scaffold(
 
+    val horizontalSpacing = 16.dp
 
-    ){ padding ->
+    val viewModel: LoginScreenViewModel = hiltViewModel()
+    val uiState = viewModel.uiState.collectAsState().value
+
+    /**
+     * Define events
+     */
+    val onUrlChanged: (String) -> Unit = { viewModel.onUrlChanged(it) }
+    val onUsernameChanged: (String) -> Unit = { viewModel.onUsernameChanged(it) }
+    val onPasswordChanged: (String) -> Unit = { viewModel.onPasswordChanged(it) }
+    val onToggleShowPassword: () -> Unit = { viewModel.onToggleShowPassword() }
+    val onToggleAllowUnencryptedConnection: () -> Unit = { viewModel.onToggleAllowUnencryptedConnection() }
+
+    Scaffold() { padding ->
 
         Column(
             modifier = Modifier
-                .padding(vertical = 64.dp, horizontal = 16.dp)
+                .padding(padding)
+                .padding(vertical = 32.dp, horizontal = 16.dp)
                 .fillMaxWidth()
                 .fillMaxHeight(),
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
 
-            AdaptiveIcon(150.dp)
-            OutlinedTextField(
-                value = "",
-                onValueChange = {},
-                label = { Text("Readeck Instance URL") },
-                modifier = Modifier.fillMaxWidth()
+            AdaptiveReadeckIcon(128.dp)
+
+            Text(
+                text = "Welcome to ReadeckApp",
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = Modifier.padding(top = 16.dp)
             )
-            OutlinedTextField(
-                value = "",
-                onValueChange = {},
-                label = { Text("Username") },
-                modifier = Modifier.fillMaxWidth()
+
+            Text(
+                text = "Please log in to your Readeck instance.",
+                style = MaterialTheme.typography.bodyLarge,
             )
-            OutlinedTextField(
-                value = "",
-                onValueChange = {},
-                label = { Text("Password") },
+
+            Column(){
+
+                TextField(
+                    value = uiState.url,
+                    onValueChange = { onUrlChanged(it) },
+                    label = { Text("Instance URL *") },
+                    prefix = { Text("https://") },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Outlined.Cloud,
+                            contentDescription = null,
+                        )
+                    },
+                    isError = uiState.urlError != null,
+                    supportingText = { Text("") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                TextField(
+                    value = uiState.username,
+                    onValueChange = { onUsernameChanged(it) },
+                    label = { Text("Username *") },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Outlined.Person,
+                            contentDescription = null,
+                        )
+                    },
+                    isError = uiState.usernameError != null,
+                    supportingText = { Text("") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                TextField(
+                    value = uiState.password,
+                    onValueChange = { onPasswordChanged(it) },
+                    label = { Text("Password *") },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Outlined.Password,
+                            contentDescription = null,
+                        )
+                    },
+                    trailingIcon = {
+                        Icon(
+                            imageVector = if(uiState.showPassword) {
+                                Icons.Filled.Visibility
+                            } else {
+                                Icons.Outlined.VisibilityOff
+                            },
+                            contentDescription = null,
+                            modifier = Modifier.clickable(
+                                enabled = true,
+                                onClick = { onToggleShowPassword() }
+                            )
+                        )
+                    },
+                    isError = uiState.passwordError != null,
+                    supportingText = { Text("") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
                 modifier = Modifier.fillMaxWidth()
-            )
+            ) {
+                Text(
+                    text = "Allow unencrypted connection",
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(end = horizontalSpacing)
+                )
+                Switch(
+                    checked = uiState.allowUnencryptedConnection,
+                    onCheckedChange = { onToggleAllowUnencryptedConnection() }
+                )
+            }
+
+            if(uiState.allowUnencryptedConnection) {
+                Row(
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    Text(
+                        text = "Unencrypted connections via HTTP are not recommended. Your credentials may be exposed!",
+                        color = MaterialTheme.colorScheme.error
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Button(
+                onClick = {}
+            ) {
+                Text("Login")
+            }
         }
     }
 }
 
-@Composable
-fun AdaptiveIcon(size: Dp = 128.dp) {
-    val bgColor = colorResource(id = R.color.ic_launcher_background) // Use your background color
-    val icon = painterResource(id = R.drawable.ic_launcher_foreground)
-
-    Surface(
-        modifier = Modifier.size(size),
-        color = bgColor,
-        shape = RoundedCornerShape(16.dp),
-        shadowElevation = 4.dp
-    ){
-        Image(
-            painter = icon,
-            contentDescription = "App Launcher Foreground Icon",
-            modifier = Modifier
-                .fillMaxSize()
-                .scale(1.5f)
-        )
-
-    }
-}
 @Composable
 @Preview
 fun LoginScreenPreview() {
--- app/src/main/java/de/readeckapp/ui/login/LoginScreenViewModel.kt
+++ app/src/main/java/de/readeckapp/ui/login/LoginScreenViewModel.kt
@@ -1,4 +1,63 @@
 package de.readeckapp.ui.login
 
-class LoginScreenViewModel {
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import de.readeckapp.domain.usecase.AuthenticateUseCase
+import de.readeckapp.domain.usecase.AuthenticationResult
+import de.readeckapp.io.prefs.SettingsDataStore
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginScreenViewModel @Inject constructor (
+    private val settingsDataStore: SettingsDataStore,
+    private val authenticateUseCase: AuthenticateUseCase
+
+) : ViewModel() {
+
+    /**
+     * Declare UI State
+     */
+    private val _uiState = MutableStateFlow(LoginUiState())
+    val uiState = _uiState.asStateFlow()
+
+    fun onUrlChanged(url: String) {
+
+    }
+
+    fun onUsernameChanged(username: String) {
+
+    }
+
+    fun onPasswordChanged(password: String) {
+
+    }
+
+    fun onToggleShowPassword() {
+        _uiState.update {
+            it.copy(showPassword = !it.showPassword)
+        }
+    }
+
+    fun onToggleAllowUnencryptedConnection() {
+        _uiState.update {
+            it.copy(allowUnencryptedConnection = !it.allowUnencryptedConnection)
+        }
+    }
+
 }
+
+data class LoginUiState(
+    val url: String = "",
+    val username: String = "",
+    val password: String = "",
+    val loginEnabled: Boolean = false,
+    val urlError: Int? = null,
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val authenticationResult: AuthenticationResult? = null,
+    val allowUnencryptedConnection: Boolean = false,
+    val showPassword: Boolean = false,
+)
